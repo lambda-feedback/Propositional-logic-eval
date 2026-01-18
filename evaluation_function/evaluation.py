ebc30e4789aa323f1d371ffea77581228cdbad3b
Harry <hs1823@ic.ac.uk>
2026-01-18 16:42:39 +0000

    fixed check only 1 param selected logic

--- evaluation_function/evaluation.py
+++ evaluation_function/evaluation.py
@@ -7,102 +7,6 @@ from evaluation_function.parsing.tokenizer import *
 from evaluation_function.parsing.tree_builder import *
 
 
-# def parse_response(response: str) -> tuple[bool, Formula | str]:
-
-#     response = response.strip()
-    
-#     # binaryOperators = ["↔","→","∨","∧"]
-#     # TODO: keep this mapping somewhere else for maintainability
-#     binaryOperators = {
-#         "↔" : Biconditional,
-#         "→" : Implication,
-#         "∨" : Disjunction,
-#         "∧" : Conjunction
-#     }
-
-#     for binaryOperator in binaryOperators.keys():
-
-#         if binaryOperator in response:
-#             split_index = response.rindex(binaryOperator)
-
-#             left  = response[:split_index]
-#             right = response[split_index+1:]
-
-#             # check left and right not empty strings
-#             if not left:
-#                 return (False, f"missing text on left of {binaryOperator}")
-#             elif not right:
-#                 return (False, f"missing text on right of {binaryOperator}")
-            
-#             parse_left = parse_response(left)
-#             parse_right = parse_response(right)
-
-#             error = False
-#             err_msgs = []
-
-#             if not parse_left[0]:
-#                 error = True
-#                 err_msgs.append(parse_left[1])
-
-#             if not parse_right[0]:
-#                 error = True
-#                 err_msgs.append(parse_right[1])
-            
-#             if error:
-#                 return (False, err_msgs.join("\n"))
-            
-#             # both sides are find and valid
-#             result = binaryOperators[binaryOperator](parse_left[1], parse_right[1])
-#             return (True, result)
-    
-
-#     # TODO: keep this mapping somewhere else for maintainability
-#     unaryOperators = {
-#         "¬" : Negation
-#     }
-
-#     for unaryOperator in unaryOperators.keys():
-
-#         #unary operator must syntactically be at the start of the string
-#         if response[0] == unaryOperator:
-            
-#             right = response[1:]
-#             #check not empty
-#             if not right:
-#                 return (False, f"missing text on right of {unaryOperator}")
-
-#             parse_right = parse_response(right)
-#             if not parse_right[0]:
-#                 return parse_right
-            
-#             result = unaryOperators[unaryOperator](parse_right[1])
-#             return (True, result)
-    
-#     # check if the formual is just True or Falsity
-    
-#     singletons = { # not sure what the official term for these symbols is
-#         "⊤" : Truth,
-#         "⊥" : Falsity
-#     }
-
-#     for singleton in singletons:
-
-#         if len(response) > 1 and singleton in response:
-#             return (False, f"not allowed to use {singleton} in the atom identifier")
-
-#         elif response == singleton:
-#             result = singletons[singleton]()
-#             return (True, result)
-    
-#     # response is likely an atom identifier
-    
-#     return (True, Atom(response))
-
-
-        
-    
-
-
 def evaluation_function(
     response: Any,
     answer: Any,
@@ -149,21 +53,21 @@ def evaluation_function(
     tautology = params.get("tautology", False)
     satisfiability = params.get("satisfiability", False)
 
-    #check that 1 and only 1 param is selected
-    if not (equivalence ^ tautology ^ satisfiability):
 
-        if not (equivalence or tautology or satisfiability):
-            #no params selected
-            return Result(
-                is_correct=False,
-                feedback_items=[("invalid param", "please select a param")]
-            )
-        
-        # more than one param selected
+    #check that 1 and only 1 param is selected
+    num_selected = sum([equivalence, tautology, satisfiability])
+
+    if num_selected == 0:
+        return Result(
+            is_correct=False,
+            feedback_items=[("invalid param", "please select a param")]
+        )
+    elif num_selected > 1:
         return Result(
             is_correct=False,
             feedback_items=[("invalid param", "please only select 1 param")]
         )
+
 
     
     feedback   = None
